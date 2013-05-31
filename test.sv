241e4fb08c9abd66784a07a38ca5a2b239d270be
Leon <auzyze@gmail.com>
2013-05-31 17:53:40 +0800

    Update test.sv

--- test.sv
+++ test.sv
@@ -1,7 +1,6 @@
 
 
 
-
 class my_test extends uvm_test;
   `uvm_component_utils(my_test)
   
@@ -12,11 +11,20 @@ class my_test extends uvm_test;
   function void build;
     super.build();
     begin
+    
+      //A test can: 
+      //Set the contents of configurations, 
+      //which are then used to control the generation of the component hierarchy from the build function
+    
       my_config cfg = new;
+      
+      //set content of configuration, used to configure components with "set_config_object"
       cfg.afull_wtrline = 12;
       cfg.aempt_wtrline = 2;
       
+      
       set_config_object("*.*component_needs_to_be_configured*", "cfg", config, 0); // ??????????
+    
     end
     
     env = top::type_id::create("env",this);  // what kind of hierarchy does "top" indicate?
