157680c4fca9a6da4b4fd53e50a4217e5f5ccebf
Leon <auzyze@gmail.com>
2013-06-06 10:26:46 +0800

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -6,10 +6,10 @@
 `define GUARD_INTERFACE
 
 /////////////////////////////////////////////////////////////
-// INTERFACE OF WRITING PORT OF SYNC FIFO
+// INTERFACE OF ASYNC FIFO
 /////////////////////////////////////////////////////////////
 
-interface wr_interface(input bit clock);
+interface fifo_interface(input bit wr_clock,rd_clock);
 
 parameter setup_time = 5ns;
 parameter hold_time = 3ns;
@@ -17,14 +17,43 @@ parameter hold_time = 3ns;
 wire  [7:0]     wr_data;
 wire            wr_en;
 
-clocking cb@(posedge clock);
+wire            rd_en;
+wire  [7:0]     rd_data;
+
+wire            full;
+wire            empty;
+wire            afull;
+wire            aempty;
+
+reg             reset;
+
+
+//clocking and modport for writing port of ASYNC FIFO
+//
+clocking cb_w@(posedge wr_clock);
 default input #setup_time output #hold_time;
-output wr_data;
-output wr_en;
-endclocking:cb;
+output  wr_data;
+output  wr_en;
+input   full;
+input   afull;
+endclocking:cb_w;
 
-modport WR(clocking cb, input clock);
+modport WR(clocking cb_w, input wr_clock);
 
-endinterface :wr_interface
+
+//clocking and modport for reading port of ASYNC FIFO
+//
+clocking cb_r@(posedge rd_clock);
+default input #setup_time output #hold_time;
+output  rd_data;
+output  rd_en;
+input   empty;
+input   aempty;
+endclocking:cb_r;
+
+modport RD(clocking cb_r, input rd_clock);
+
+
+endinterface:fifo_interface
 
 `endif
